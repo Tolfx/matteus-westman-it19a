0d3d4031d713a5103b553f369db692537a94bd29
Tolfx <matteus12122@gmail.com>
2020-10-13 09:17:13 +0200

    ✔ funktioner

---
+++ Programming1/funktioner/index.js
@@ -0,0 +1,18 @@
+function dividera(tal1, tal2) 
+{
+    return tal1/tal2;
+}
+
+function multi(tal1, tal2) 
+{
+    return tal1*tal2;
+}
+
+function subtra(tal1, tal2) 
+{
+    return tal1+tal2;
+}
+
+console.log(dividera(12, 10))
+console.log(multi(142857, 6))
+console.log(subtra(1, 1))
