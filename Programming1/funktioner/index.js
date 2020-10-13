66ca0f54d33eda29a356b7e608117262d33adc80
Tolfx <matteus12122@gmail.com>
2020-10-13 09:36:38 +0200

    ✔ Modified < index.js >

--- Programming1/funktioner/index.js
+++ Programming1/funktioner/index.js
@@ -1,5 +1,8 @@
 function dividera(tal1, tal2) 
 {
+    if(tal2 === 0)
+        return "Invalid, can't divide 0, too strong!!!";
+    
     return tal1/tal2;
 }
 
@@ -13,6 +16,6 @@ function subtra(tal1, tal2)
     return tal1+tal2;
 }
 
-console.log(dividera(12, 10))
+console.log(dividera(12, 0))
 console.log(multi(142857, 6))
 console.log(subtra(1, 1))
