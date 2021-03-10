a23b7868edb052dc56733232eb7eb34127928523
Tolfx <matteus12122@gmail.com>
2021-03-10 11:05:26 +0100

    CV

---
+++ Tillämpadprog/wesmat_website/sites/bootstrap/js/script.js
@@ -0,0 +1,41 @@
+const Om_Mig = document.querySelector("#om-mig");
+const Bilder = document.querySelector("#bilder");
+const Fun_Facts = document.querySelector("#fun-facts");
+const Content = document.querySelector("#content");
+
+/**
+ * 
+ * @param {"om-mig" | "bilder" | "fun-facts"} dom 
+ */
+const changeOpacity = (dom) => {
+    if(dom.match(/om-mig|bilder|fun-facts/g))
+    {
+        switch(dom)
+        {
+            case "om-mig": {
+                console.log("Showing Om-Mig");
+                Content.style.paddingBottom = "15rem";
+                Om_Mig.style.opacity = "1";
+                Bilder.style.opacity = "0";
+                Fun_Facts.style.opacity = "0";
+                break;
+            }
+            case "bilder": {
+                console.log("Showing Bilder");
+                Content.style.paddingBottom = "29rem";
+                Om_Mig.style.opacity = "0";
+                Bilder.style.opacity = "1";
+                Fun_Facts.style.opacity = "0";
+                break;
+            }
+            case "fun-facts": {
+                console.log("Showing Fun-Facts");
+                Content.style.paddingBottom = "2rem";
+                Om_Mig.style.opacity = "0";
+                Bilder.style.opacity = "0";
+                Fun_Facts.style.opacity = "1";
+                break;
+            }
+        }
+    }
+};
